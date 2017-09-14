119ad223f254da7a43b80696b28d3ea97df4ed4e
Jami Lehtinen <jami.lehtinen1@gmail.com>
2017-09-14 14:05:44 +0300

    Netbeans projekti luotu ja tutustuttu viikkotehtävään

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,17 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+    class HelloWorldController extends BaseController{
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
-    }
+        public static function index(){
+            // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+            //View::make('home.html');
+
+            echo 'Tämä on etusivu!';
+        }
+
+        public static function sandbox(){
+            // Testaa koodiasi täällä
+            View::make('helloworld.html');
+        }
     }
-  }
