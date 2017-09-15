aabc258da1dfeaeb8ee1b5c78d3dc299b815ced9
Jami Lehtinen <jami.lehtinen1@gmail.com>
2017-09-15 16:56:33 -0400

    html tiedostot alustettu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -5,13 +5,18 @@
         public static function index(){
             // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
 
-            //View::make('home.html');
+            View::make('login.html');
+        }
+        
+        public static function register(){
+            // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
 
-            echo 'Tämä on etusivu!';
+            View::make('register.html');
         }
 
         public static function sandbox(){
             // Testaa koodiasi täällä
-            View::make('helloworld.html');
+            View::make('login.html');
+            //View::make('helloworld.html');
         }
     }
--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,10 @@
   $routes->get('/', function() {
     HelloWorldController::index();
   });
+  
+  $routes->get('/register', function() {
+    HelloWorldController::register();
+  });
 
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
